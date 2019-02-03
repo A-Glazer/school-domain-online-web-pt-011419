a21e4e318cbe4264357e52eac7fc755f4d185ddf
A Glazer <avigayilglazer@gmail.com>
2019-02-03 19:38:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,14 @@
 class School
   attr_reader :school_name, :roster
 
-def initialize(school)
-
-  school.roster
+def initialize(school_name)
+  @school_name = school_name
+  @roster = {}
 end
+  
+def add_student(student_name, grade)
+  @roster[grade] = []
+  @roster[grade] << student_name
 
 
 end
